859612f67aa15b3634c6e4e5f72be53f3f90f1e6
Nancy Fan <hmfnj1996@gmail.com>
2016-11-04 20:14:32 +0800

    decode riscv64 instruction

--- RISCV_ISA_SIM/decode.cpp
+++ RISCV_ISA_SIM/decode.cpp
@@ -7,3 +7,40 @@
 //
 
 #include "decode.hpp"
+
+bool instruction::getType(){
+    xcode op = (ins)inst&OPCODE;
+    switch(op)
+    {
+    case 0x33: // b0110011
+        optype =  R_TYPE;
+            return true;
+        
+    case 0x67: // b1100111
+    case 0x03: // b0000011
+    case 0x13: // b0010011
+    case 0x73: // b1110011
+        optype =  I_TYPE;
+            return true;
+        
+    case 0x23: // b0100011
+        optype =  S_TYPE;
+            return true;
+        
+    case 0x63: // b1100011
+        optype =  SB_TYPE;
+            return true;
+        
+    case 0x37: // b0110111
+    case 0x17: // b0010111
+        optype =  U_TYPE;
+            return true;
+        
+    case 0x6F: // b1101111
+        optype =  UJ_TYPE;
+            return true;
+        
+    default:
+            return false;
+    }
+}
